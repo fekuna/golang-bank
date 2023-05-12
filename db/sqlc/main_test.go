1e6f003c8ea3ac03ffa57953dfbc5e0ae59cd9e9
Alfan <alfan.almunawar@yummycorp.com>
2023-05-12 15:26:19 +0700

    golang-bank

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/gobank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	fmt.Println("TestMain Running...")
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
