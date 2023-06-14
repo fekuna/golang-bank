d3d41aa2728e54fa766aea7c21a168e0fac2d4cd
alfan <almunawar.alfan@gmail.com>
2023-06-14 17:24:23 +0700

    [Backend #14] Implement transfer money API with a custom params validator

--- api/server.go
+++ api/server.go
@@ -25,6 +25,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
 
 	return server
