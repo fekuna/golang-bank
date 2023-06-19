1eacb1dcaf4fac050171990d0236228f45088141
alfan <almunawar.alfan@gmail.com>
2023-06-19 21:38:13 +0700

    19

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/fekuna/golang-bank/db/sqlc"
+	"github.com/fekuna/golang-bank/token"
+	"github.com/fekuna/golang-bank/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,14 +13,23 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     utils.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
-func NewServer(store db.Store) *Server {
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
 	server := &Server{
-		store: store,
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
 	}
 	router := gin.Default()
 
@@ -37,7 +50,7 @@ func NewServer(store db.Store) *Server {
 
 	server.router = router
 
-	return server
+	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
